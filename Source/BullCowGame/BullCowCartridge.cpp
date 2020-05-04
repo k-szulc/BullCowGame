3da3ef486c3e0a7e0d017a081d4420856206c39a
kszulc <s-q-n@protonmail.com>
2020-05-04 10:27:08 +0200

    moved logic to new ProcessGuess function

--- Source/BullCowGame/BullCowCartridge.cpp
+++ Source/BullCowGame/BullCowCartridge.cpp
@@ -4,61 +4,21 @@
 void UBullCowCartridge::BeginPlay() // When the game starts
 {
     Super::BeginPlay();
-    SetupGame(); 
+    SetupGame();
 }
 
-void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
+void UBullCowCartridge::OnInput(const FString &Input) // When the player hits enter
 {
 
-    if  (bGameOver)
+    if (bGameOver)
     {
         ClearScreen();
         SetupGame();
     }
     else
     {
-        if (Input == HiddenWord) 
-        {
-            PrintLine(TEXT("Bingo !"));
-            EndGame();
-        } 
-        else 
-        {
-            PrintLine(TEXT("Nope."));
-            
-            if (Input.Len() != HiddenWord.Len())
-            {
-                PrintLine(TEXT("Hidden Word is %i chars long You idiot, try again !"), HiddenWord.Len());
-            }
-            else
-            {
-                int64 CorrectLetters = 0;
-
-                for(int a = 0 ; a < HiddenWord.Len() ; a++) 
-                {
-                    
-                    if(Input[a] == HiddenWord[a])
-                    {
-                        ++CorrectLetters;
-                    }
-
-                }
-
-                PrintLine(TEXT("In Your stuppid attempt You had %i correct letters, and %i wrong. Get better kid! "), CorrectLetters, HiddenWord.Len() - CorrectLetters);
-            }
-            
-            PrintLine(TEXT("You have %i lives left."), --PlayerLives);
-
-            if (PlayerLives == 0)
-            {
-                PrintLine(TEXT("You fuckin lost mate."));
-                EndGame();
-                
-            }
-
-        }
+        ProcessGuess(Input);
     }
-
 }
 
 void UBullCowCartridge::SetupGame()
@@ -70,7 +30,6 @@ void UBullCowCartridge::SetupGame()
     PrintLine(TEXT("The HiddenWord is: %s"), *HiddenWord); // debug line
 
     bGameOver = false;
-    
 
     PrintLine(TEXT("Welcome into Bull and Cows game."));
     PrintLine(TEXT("Guess the %i letter word"), HiddenWord.Len());
@@ -83,4 +42,45 @@ void UBullCowCartridge::EndGame()
 {
     bGameOver = true;
     PrintLine(TEXT("Please press ENTER to play again."));
+}
+
+void UBullCowCartridge::ProcessGuess(FString Guess)
+{
+    if (Guess == HiddenWord)
+    {
+        PrintLine(TEXT("Bingo !"));
+        EndGame();
+    }
+    else
+    {
+        PrintLine(TEXT("Nope."));
+
+        if (Guess.Len() != HiddenWord.Len())
+        {
+            PrintLine(TEXT("Hidden Word is %i chars long You idiot, try again !"), HiddenWord.Len());
+        }
+        else
+        {
+            int64 CorrectLetters = 0;
+
+            for (int a = 0; a < HiddenWord.Len(); a++)
+            {
+
+                if (Guess[a] == HiddenWord[a])
+                {
+                    ++CorrectLetters;
+                }
+            }
+
+            PrintLine(TEXT("In Your stuppid attempt You had %i correct letters, and %i wrong. Get better kid! "), CorrectLetters, HiddenWord.Len() - CorrectLetters);
+        }
+
+        PrintLine(TEXT("You have %i lives left."), --PlayerLives);
+
+        if (PlayerLives == 0)
+        {
+            PrintLine(TEXT("You fuckin lost mate."));
+            EndGame();
+        }
+    }
 }
